f79e06da33765f2082b9bc26077d648705e8283b
graziellecafe <graziellecafe@users.noreply.github.com>
2021-03-16 09:33:07 -0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,17 @@
+function saudacao(nome){
+    console.log(`Bom dia ${nome}!`);
+}
+saudacao('Grazielle')
+
+
+function soma(x,y){
+    const resultado = x + y; 
+    return resultado
+}
+
+console.log(soma(4,5));
+
+
+
+
+
